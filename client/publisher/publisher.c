972b2f482606e3f06e38f1f0ba85ee7a518dd701
Instant Contiki <vincenzo.consales95@gmail.com>
2018-12-20 13:18:12 +0100

    added resource definition (may be useful in future)

--- client/publisher/publisher.c
+++ client/publisher/publisher.c
@@ -4,19 +4,64 @@
 #include "sys/etimer.h"
 #include "net/ip/uip.h"
 #include "net/ipv6/uip-ds6.h"
-
 #include "simple-udp.h"
-
-
+#include "contiki-net.h"
+#include "rest-engine.h"
 #include <stdio.h>
 #include <string.h>
 
-#define UDP_PORT 1111
+#define UDP_PORT 5683
 
 #define SEND_INTERVAL   (2 * CLOCK_SECOND)
 #define SEND_TIME   (random_rand() % (SEND_INTERVAL))
 
-static struct simple_udp_connection broadcast_connection;
+#define DEBUG 0
+#if DEBUG
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+/*
+ * A handler function must be implemented for each RESOURCE.
+ * A buffer for the response payload is provided through the buffer pointer. Simple resources can ignore
+ * preferred_size and offset, but must respect the REST_MAX_CHUNK_SIZE limit for the buffer.
+ * If a smaller block size is requested for CoAP, the REST framework automatically splits the data.
+ */
+void
+helloworld_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  const char *len = NULL;
+  /* Some data that has the length up to REST_MAX_CHUNK_SIZE. For more, see the chunk resource. */
+  char const * const message = "Hello World! ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
+  int length = 12; /*           |<-------->| */
+
+  /* The query string can be retrieved by rest_get_query() or parsed for its key-value pairs. */
+  if (REST.get_query_variable(request, "len", &len)) {
+    length = atoi(len);
+    if (length<0) length = 0;
+    if (length>REST_MAX_CHUNK_SIZE) length = REST_MAX_CHUNK_SIZE;
+    memcpy(buffer, message, length);
+  } else {
+    memcpy(buffer, message, length);
+  }
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN); /* text/plain is the default, hence this option could be omitted. */
+  REST.set_header_etag(response, (uint8_t *) &length, 1);
+  REST.set_response_payload(response, buffer, length);
+}
+
+RESOURCE(helloworld,
+         "title=\"Hello world: ?len=0..\";rt=\"Text\"",
+         helloworld_handler,
+         NULL,
+         NULL,
+         NULL);
+
+static struct simple_udp_connection connection;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(publisher, "publisher example process");
@@ -32,11 +77,13 @@ receiver(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  printf("Data received from ");
-  uip_debug_ipaddr_print(sender_addr);
+	printf("Data received from ");
+	uip_debug_ipaddr_print(sender_addr);
 
-  printf(" on port %d from port %d with length %d\n",
+	printf(" on port %d from port %d with length %d\n",
          receiver_port, sender_port, datalen);
+   printf("data sending reply\n");
+   simple_udp_sendto(c, "Reply", strlen("Reply") + 1, sender_addr);
 }
 /*---------------------------------------------------------------------------*/
 static void
@@ -68,15 +115,23 @@ PROCESS_THREAD(publisher, ev, data)
 {
 	static struct etimer periodic_timer;
   	static struct etimer send_timer;
-  	uip_ipaddr_t addr;
+  	uip_ipaddr_t broker_addr;
 
 	PROCESS_BEGIN();
 
 	set_global_address();
-	simple_udp_register(&broadcast_connection, UDP_PORT,
+	simple_udp_register(&connection, UDP_PORT,
 	                  NULL, UDP_PORT,
 	                  receiver);
+	uip_ip6addr(&addr, 0xfe80, 0, 0, 0, 0xc30c, 0, 0, 0x0001); //broker address??
 
+	 /* Initialize the REST engine. */
+	rest_init_engine();
+
+  	/* Activate the application-specific resources. */
+	rest_activate_resource(&helloworld, "hello");
+
+	simple_udp_sendto(&connection, "Test", 4, &addr);
 	etimer_set(&periodic_timer, SEND_INTERVAL);
 	while(1) {
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
@@ -86,7 +141,7 @@ PROCESS_THREAD(publisher, ev, data)
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
 		printf("Sending broadcast\n");
 		uip_create_linklocal_allnodes_mcast(&addr);
-		simple_udp_sendto(&broadcast_connection, "Test", 4, &addr);
+		simple_udp_sendto(&connection, "Test", 4, &addr);
 	}
 
 	PROCESS_END();
