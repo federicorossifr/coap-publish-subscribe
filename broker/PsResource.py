4609165784105fd97b015bb7b45ab653b4841370
Federico Rossi <f.rossi57@studenti.unipi.it>
2018-12-20 12:05:39 +0100

    Added regex to check CoRe format correctness. Need to fix attribute set

--- broker/PsResource.py
+++ broker/PsResource.py
@@ -1,6 +1,7 @@
 from coapthon.resources.resource import Resource
 from coapthon import defines
-
+import re
+pattern = r'^<topic>;(?:(ct=\w+;)|(rt=\w+;)|(rel=\w+;)|(hreflang=\w+;)|(media=\w+;)|(title=\w+;)|(if=\w+;))+$'
 def delete_subtree(root_resource,base=False):
     if(len(root_resource.children) == 0):
         print("Deleting: "+root_resource.name)
@@ -16,24 +17,16 @@ def delete_subtree(root_resource,base=False):
         	return        
 
 
-def parsePostPayload(payload):
-    pairs = payload.split("&");
-    payload_dict = {}
-    for pair in pairs:
-        key_val = pair.split("=")
-        payload_dict[key_val[0]] = key_val[1]
-        pass
-    return payload_dict
+
 
 class PsResource(Resource):
     def __init__(self, name="PsResource",coap_server=None):
         super(PsResource, self).__init__(name, coap_server, visible=True,
                                             observable=True, allow_children=True)
         self.cs = coap_server
-        self.resource_type = "rt1"
+        self.resource_type = "core.ps"
         self.content_type = "text/plain"
-        self.interface_type = "if1"
-        self.payload = "Basic Resource"
+        self.payload = ""
         self.children = []
 
     def render_GET_advanced(self, request, response):
@@ -42,19 +35,39 @@ class PsResource(Resource):
         response.code = defines.Codes.CONTENT.number
         return self, response
 
+    def createResFromPayload(self,payload,base):
+        if(payload is None or not re.match(r'^<(\w+)>;(?:(ct=\w+;)|(rt=\w+;)|(rel=\w+;)|(hreflang=\w+;)|(media=\w+;)|(title=\w+;)|(if=\w+;))+$',payload)):
+            return None
+        payload = payload[:-1]
+        topicData = payload.split(";")
+        topicPath = topicData[0]
+        path = topicPath.replace("<","").replace(">","")
+        resource = PsResource(base+"/"+path,self.cs)
+        topicData.pop(0);
+        for d in topicData:
+            keyval = d.split("=")
+            print(keyval)
+            resource._attributes[keyval[0]] = keyval[1]
+        return resource
+
+
     def render_POST_advanced(self, request, response):
-        #CHECK PAYLOAD FORMAT
-        payload = parsePostPayload(request.payload)
-        print(request.uri_path)
-        child_res = PsResource(request.uri_path+"/"+payload["topic"],self.cs)
+        child_res = self.createResFromPayload(request.payload,request.uri_path)
+        if(child_res is None):
+            response.code = defines.Codes.BAD_REQUEST.number
+            return self,response
         self.children.append(child_res)
-        self.cs.add_resource(request.uri_path+"/"+payload["topic"],child_res)
+        print(child_res.name)   
+        self.cs.add_resource(child_res.name,child_res)
         response.code = defines.Codes.CREATED.number
         return self,response
 
     def render_PUT_advanced(self, request, response):
+        if(request.uri_path == "ps"):
+            print("FORBIDDEN")
+            response.code = defines.Codes.FORBIDDEN.number
+            return False, response        
         self.payload = request.payload
-        from coapthon.messages.response import Response
         response.payload = "Response changed through PUT"
         response.code = defines.Codes.CHANGED.number
         return self, response
