bdfb8d92f508b0a1913a995541fab4aafc5b8d8a
Instant Contiki <vincenzo.consales95@gmail.com>
2018-12-20 22:55:54 +0100

    added rpl-border-router for sky

---
+++ rpl-border-router/border-router.c
@@ -0,0 +1,182 @@
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+/**
+ * \file
+ *         border-router
+ * \author
+ *         Niclas Finne <nfi@sics.se>
+ *         Joakim Eriksson <joakime@sics.se>
+ *         Nicolas Tsiftes <nvt@sics.se>
+ */
+
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+#include "net/ip/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/rpl/rpl.h"
+#include "net/rpl/rpl-private.h"
+#if RPL_WITH_NON_STORING
+#include "net/rpl/rpl-ns.h"
+#endif /* RPL_WITH_NON_STORING */
+#include "net/netstack.h"
+#include "dev/button-sensor.h"
+#include "dev/slip.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define DEBUG DEBUG_NONE
+#include "net/ip/uip-debug.h"
+
+static uip_ipaddr_t prefix;
+static uint8_t prefix_set;
+
+PROCESS(border_router_process, "Border router process");
+
+
+AUTOSTART_PROCESSES(&border_router_process);
+
+#if BUF_USES_STACK
+static char *bufptr, *bufend;
+#define ADD(...) do {                                                   \
+    bufptr += snprintf(bufptr, bufend - bufptr, __VA_ARGS__);      \
+  } while(0)
+#else
+static char buf[256];
+static int blen;
+#define ADD(...) do {                                                   \
+    blen += snprintf(&buf[blen], sizeof(buf) - blen, __VA_ARGS__);      \
+  } while(0)
+#endif
+
+/*---------------------------------------------------------------------------*/
+static void
+print_local_addresses(void)
+{
+  int i;
+  uint8_t state;
+
+  PRINTA("Server IPv6 addresses:\n");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      PRINTA(" ");
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      PRINTA("\n");
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+void
+request_prefix(void)
+{
+  /* mess up uip_buf with a dirty request... */
+  uip_buf[0] = '?';
+  uip_buf[1] = 'P';
+  uip_len = 2;
+  slip_send();
+  uip_clear_buf();
+}
+/*---------------------------------------------------------------------------*/
+void
+set_prefix_64(uip_ipaddr_t *prefix_64)
+{
+  rpl_dag_t *dag;
+  uip_ipaddr_t ipaddr;
+  memcpy(&prefix, prefix_64, 16);
+  memcpy(&ipaddr, prefix_64, 16);
+  prefix_set = 1;
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  dag = rpl_set_root(RPL_DEFAULT_INSTANCE, &ipaddr);
+  if(dag != NULL) {
+    rpl_set_prefix(dag, &prefix, 64);
+    PRINTF("created a new RPL dag\n");
+  }
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(border_router_process, ev, data)
+{
+  static struct etimer et;
+
+  PROCESS_BEGIN();
+
+/* While waiting for the prefix to be sent through the SLIP connection, the future
+ * border router can join an existing DAG as a parent or child, or acquire a default
+ * router that will later take precedence over the SLIP fallback interface.
+ * Prevent that by turning the radio off until we are initialized as a DAG root.
+ */
+  prefix_set = 0;
+  NETSTACK_MAC.off(0);
+
+  PROCESS_PAUSE();
+
+  SENSORS_ACTIVATE(button_sensor);
+
+  PRINTF("RPL-Border router started\n");
+#if 0
+   /* The border router runs with a 100% duty cycle in order to ensure high
+     packet reception rates.
+     Note if the MAC RDC is not turned off now, aggressive power management of the
+     cpu will interfere with establishing the SLIP connection */
+  NETSTACK_MAC.off(1);
+#endif
+
+  /* Request prefix until it has been received */
+  while(!prefix_set) {
+    etimer_set(&et, CLOCK_SECOND);
+    request_prefix();
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  }
+
+  /* Now turn the radio on, but disable radio duty cycling.
+   * Since we are the DAG root, reception delays would constrain mesh throughbut.
+   */
+  NETSTACK_MAC.off(1);
+
+#if DEBUG || 1
+  print_local_addresses();
+#endif
+
+  while(1) {
+    PROCESS_YIELD();
+    if (ev == sensors_event && data == &button_sensor) {
+      PRINTF("Initiating global repair\n");
+      rpl_repair_root(RPL_DEFAULT_INSTANCE);
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
