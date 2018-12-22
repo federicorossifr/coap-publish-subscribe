6feca9be902b696a9d107705f30724c356ae1219
Instant Contiki <vincenzo.consales95@gmail.com>
2018-12-22 13:10:46 +0100

    added topics ps/sensors/accelerometer. Added simple example that updates topic

--- client/publisher/publisher.c
+++ client/publisher/publisher.c
@@ -1,22 +1,17 @@
 #include "contiki.h"
-#include "lib/random.h"
-#include "sys/ctimer.h"
 #include "sys/etimer.h"
 #include "net/ip/uip.h"
 #include "net/ipv6/uip-ds6.h"
-#include "simple-udp.h"
 #include "contiki-net.h"
-#include "rest-engine.h"
 #include "er-coap-engine.h"
 #include <stdio.h>
 #include <string.h>
 
-#define PERIOD 10
 #define LOCAL_PORT      UIP_HTONS(COAP_DEFAULT_PORT + 1)
 #define REMOTE_PORT     UIP_HTONS(COAP_DEFAULT_PORT)
 
-#define DEBUG 0
-#if DEBUG
+#define DEBUG 
+#ifdef DEBUG
 #define PRINTF(...) printf(__VA_ARGS__)
 #define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
 #define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
@@ -26,11 +21,9 @@
 #define PRINTLLADDR(addr)
 #endif
 
-/* Example URIs that can be queried. */
-#define NUMBER_OF_URLS 5
 /* leading and ending slashes only for demo purposes, get cropped automatically when setting the Uri-Path */
 char *service_urls[] =
-{ ".well-known/core", "ps", "ps/topic1", "/ps/sensors", "ps/sensors/accelorometer"};
+{ ".well-known/core", "ps", "ps/sensors", "ps/sensors/accelorometer"};
 
 /*---------------------------------------------------------------------------*/
 PROCESS(publisher, "publisher example process");
@@ -82,8 +75,13 @@ client_chunk_handler(void *response)
   printf("|%.*s", len, (char *)chunk);
 }
 
-static void create_topic(const uip_ipaddr_t *broker_addr, const char *topic_name, const char *service_url, coap_packet_t *request)
+static 
+void create_topic(const uip_ipaddr_t *broker_addr, 
+				  const char *service_url, 
+				  const char *topic_name, 
+				  coap_packet_t *request)
 {  
+	// implementare il secure coding
 	char msg[256];
 	memset(msg,0,256);
 	/* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
@@ -98,10 +96,25 @@ static void create_topic(const uip_ipaddr_t *broker_addr, const char *topic_name
 	coap_set_payload(request, (uint8_t *)msg, strlen(msg));
 }
 
+static 
+void update_topic(const uip_ipaddr_t *broker_addr, 
+				  const char *service_url, 
+				  const char *new_value, 
+				  uint8_t len,
+				  coap_packet_t *request)
+{
+	coap_init_message(request, COAP_TYPE_CON, COAP_PUT, 0);
+	coap_set_header_uri_path(request, service_url);
+
+	coap_set_payload(request, (uint8_t *)new_value, len);
+}
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(publisher, ev, data)
 {
-	static int created = 0;
+	static uint16_t i = -1;
+#define DIM 8
+	static char buf[DIM];
 	static struct etimer periodic_timer;
 	static uip_ipaddr_t broker_addr;
 	static coap_packet_t request[1];
@@ -114,25 +127,28 @@ PROCESS_THREAD(publisher, ev, data)
 	// NETSTACK_MAC.off(1); //duty cycle del 100%
   	/* receives all CoAP messages */
   	coap_init_engine();
-	etimer_set(&periodic_timer, PERIOD * CLOCK_SECOND);
-	
-	while(1) {
-		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-		print_addresses();
-		printf("--Toggle timer--\n");
+	etimer_set(&periodic_timer, CLOCK_SECOND);
 
-		PRINTF("BROKER ADDRESS:");
-		PRINT6ADDR(&broker_ipaddr);
-		PRINTF(" : %u\n", UIP_HTONS(REMOTE_PORT));
-		
-		//if( created == 0 ){
-			create_topic(&broker_addr, "topic1", service_urls[1], request);
-			COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request,
-				            client_chunk_handler);
-			created = 1;
-		//} 
-		
+	PROCESS_WAIT_EVENT_UNTIL(PROCESS_EVENT_TIMER);
+	create_topic(&broker_addr, "ps", "sensors", request);
+	COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
+	PRINTF("topic ps/sensors created\n");
+	etimer_reset(&periodic_timer);
+	
+	PROCESS_WAIT_EVENT_UNTIL(PROCESS_EVENT_TIMER);
+	create_topic(&broker_addr, "ps/sensors", "accelorometer", request);
+	COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
+	PRINTF("topic ps/sensors/accelorometer created\n");
+	etimer_reset(&periodic_timer);
+
+	while(1) {
+		PROCESS_WAIT_EVENT_UNTIL(PROCESS_EVENT_TIMER);
+		PRINTF("I update_topic i=%d \n",i);
+		sprintf(buf,"%d",i);
+		update_topic(&broker_addr, "ps/sensors/accelorometer", buf, DIM, request);
+		COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
 		etimer_reset(&periodic_timer);
+		i++;
 	}
 
 	PROCESS_END();
