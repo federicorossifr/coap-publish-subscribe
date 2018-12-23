ecba65a447f1b338af1f25ce8d66289740582e86
Federico Rossi <f.rossi57@studenti.unipi.it>
2018-12-23 09:25:49 +0100

    Added latex files for presentation. As you prefer by the way

---
+++ presentation/compile.bat
@@ -0,0 +1,9 @@
+SET main=presentation
+@echo off
+cls
+echo PDFLATEX --- 1/2
+pdflatex  -aux-directory out/ -output-directory out/  %main%.tex  
+echo PDFLATEX --- 2/2
+pdflatex  -aux-directory out/ -output-directory out/ -quiet %main%.tex
+
+START "" out/%main%.pdf
