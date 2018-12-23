973e6fc500a5ceb362a3a6ca65ae2e88cd2f11c8
Vincenzo <vincenzo.consales95@gmail.com>
2018-12-23 16:33:30 +0100

    issue on library not solvable

--- client/commons.h
+++ client/commons.h
@@ -25,10 +25,28 @@
 /* leading and ending slashes only for demo purposes, get cropped automatically when setting the Uri-Path */
 const char *urls[] = {".well-known/core", "ps", "ps/sensors", "ps/sensors/accelorometer"};
 
-void print_addresses(void);
+void print_addresses(void){
+	int i;
+	for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+		uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+		printf("\n");
+	}
+}
+
 void update_topic(const uip_ipaddr_t *broker_addr, 
 				  char *service_url, 
 				  char *new_value, 
-				  uint8_t len);
+				  uint8_t len)
+{
+	static coap_packet_t pkt[1];
+	static uint8_t pkt_serialized[256];
+	static size_t size_pkt;
+	coap_init_message(pkt, COAP_TYPE_CON, COAP_PUT, 0);
+	coap_set_header_uri_path(pkt, service_url);
+	coap_set_payload(pkt, (uint8_t *)new_value, len);
+	size_pkt = coap_serialize_message(pkt, pkt_serialized);
+	coap_send_message(broker_addr, REMOTE_PORT, pkt_serialized, size_pkt);
+}
+
 
 #endif
