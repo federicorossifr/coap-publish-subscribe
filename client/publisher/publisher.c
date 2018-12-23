484055a4a8c0fae86bcf363eeededf027ae9bb34
Vincenzo <vincenzo.consales95@gmail.com>
2018-12-23 15:38:59 +0100

    added support to json

--- client/publisher/publisher.c
+++ client/publisher/publisher.c
@@ -48,22 +48,25 @@ client_chunk_handler(void *response)
   printf("|%.*s\n", len, (char *)chunk);
 }
 
-static char buf_create[64]; 
+#define DIM_BUF_CREATE 64
+static char buf_create[DIM_BUF_CREATE]; 
 void create_topic(const uip_ipaddr_t *broker_addr, 
 				  const char *service_url, 
-				  const char *topic_name, 
+				  const char *topic_name,
+				  const char *ct, 
 				  coap_packet_t *request)
 {  
-	memset(buf_create,0,64);
+	memset(buf_create,0,DIM_BUF_CREATE);
 	/* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
 	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
 	coap_set_header_uri_path(request, service_url);
 
 	//create!!!!!
 	//const char msg[] = "<topic1>;ct=0;";
-	strcat(buf_create, "<");
+	snprintf(buf_create, DIM_BUF_CREATE, "<%s>;ct=%s;",topic_name,ct);
+	/*strcat(buf_create, "<");
 	strcat(buf_create, topic_name);
-	strcat(buf_create, ">;ct=0;"); 
+	strcat(buf_create, ">;ct=");*/
 	printf("msg=%s l=%d\n",buf_create,strlen(buf_create));
 	coap_set_payload(request, (uint8_t *)buf_create, strlen(buf_create));
 }
@@ -83,13 +86,19 @@ void update_topic(const uip_ipaddr_t *broker_addr,
 	coap_send_message(broker_addr, REMOTE_PORT, pkt_serialized, size_pkt);
 }
 
+void 
+json_temp_msg(int16_t temp, char *buf_out, uint8_t buf_size)
+{
+	snprintf(buf_out, buf_size, "{\"temp\" : %d, \"udm\" : \"°C\" }",temp);
+}
+
 #define PERIOD 1
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(publisher, ev, data)
 {
 	PROCESS_BEGIN();	
 	static uint16_t i = -1;
-#define DIM 8
+#define DIM 48
 	static char buf[DIM];
 	static struct etimer periodic_timer;
 	static uip_ipaddr_t broker_addr;
@@ -101,20 +110,19 @@ PROCESS_THREAD(publisher, ev, data)
   	coap_init_engine();
 	printf("coap_init done\n");		  
 
-	create_topic(&broker_addr, urls[1], "sensors", request);
+	create_topic(&broker_addr, urls[1], "sensors", "0", request);
 	COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
 		
-	create_topic(&broker_addr, urls[2], "accelorometer", request);
+	create_topic(&broker_addr, urls[2], "accelorometer", "0", request);
 	COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
 
 	etimer_set(&periodic_timer, PERIOD*CLOCK_SECOND);	
 	while(1) {
 		PROCESS_WAIT_EVENT_UNTIL(PROCESS_EVENT_TIMER);
-		sprintf(buf,"%d",i);
-		i++;
+		json_temp_msg(i,buf,DIM);
 		update_topic(&broker_addr, urls[3], buf, DIM);
 		//COAP_BLOCKING_REQUEST(&broker_addr, REMOTE_PORT, request, client_chunk_handler);
-		etimer_reset(&periodic_timer);
+		i++; etimer_reset(&periodic_timer);
 	}
 
 	PROCESS_END();
