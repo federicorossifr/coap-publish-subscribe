1e3a041b95bd0655d4e69e79bb96fdc0721005e8
Vincenzo <vincenzo.consales95@gmail.com>
2018-12-24 16:28:55 +0100

    added transaction

--- client/commons.h
+++ client/commons.h
@@ -67,13 +67,19 @@ void update_topic(const uip_ipaddr_t *broker_addr,
 				  uint8_t len)
 {
 	static coap_packet_t pkt[1];
+	static coap_transaction_t transaction[1];
 	static uint8_t pkt_serialized[256];
 	static size_t size_pkt;
-	coap_init_message(pkt, COAP_TYPE_CON, COAP_PUT, 0);
+	coap_init_message(pkt, COAP_TYPE_NON, COAP_PUT, 0);
 	coap_set_header_uri_path(pkt, service_url);
 	coap_set_payload(pkt, (uint8_t *)new_value, len);
-	size_pkt = coap_serialize_message(pkt, pkt_serialized);
-	coap_send_message(broker_addr, REMOTE_PORT, pkt_serialized, size_pkt);
+	pkt->mid = coap_get_mid();
+	transaction = coap_new_transaction(pkt->mid, broker_addr, REMOTE_PORT);
+	transaction->packet_len = coap_serialize_message(request, transaction->packet);
+
+	//size_pkt = coap_serialize_message(pkt, pkt_serialized);
+	//coap_send_message(broker_addr, REMOTE_PORT, pkt_serialized, size_pkt);
+	coap_send_transaction(transaction);
 }
 
 
